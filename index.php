d54921f07a8ecb2ff08fa0adbac9005d01852fe8
lordevito <lollo.devito@hotmail.com>
2024-01-10 14:41:36 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Iscrizione Newsletter</title>
+</head>
+<body>
+<form action="" method="GET">
+  <label for="email">Inserisci la email</label>
+  <input type="email" placeholder="user@gmail.com">
+</form>
+</body>
+</html>
