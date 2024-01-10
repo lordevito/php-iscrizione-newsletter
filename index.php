38fa136ded03caa8380239ef5f992f683786a368
lordevito <lollo.devito@hotmail.com>
2024-01-10 22:56:40 +0100

    feat: milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+/* Se email esiste */
+if(isset($_GET['email'])){
+  /* Allora se contiene @ e . stmapa */
+  if(str_contains($_GET['email'], '@') && str_contains($_GET['email'] , '.')){
+    echo 'email valida';
+  }
+  /* Atrimenti mostra avviso di email non valida */
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +19,10 @@
     <title>PHP Iscrizione Newsletter</title>
 </head>
 <body>
-<form action="" method="GET">
+<form action="index.php" method="GET">
   <label for="email">Inserisci la email</label>
-  <input type="email" placeholder="user@gmail.com">
+  <input name="email" type="email" placeholder="user@gmail.com">
+  <button type="submit">Invia</button>
 </form>
 </body>
 </html>
